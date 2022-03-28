733690d295a4f58bf1b8453dc7b8a0d53a54eff6
oopsmonk <oopsmonk@gmail.com>
2022-03-29 02:36:54 +0800

    Enabled nvim-cmp autocomplete

--- init.lua
+++ init.lua
@@ -64,15 +64,15 @@ require('packer').startup(function(use)
   }
 
   -- LSP and Autocompletion
-  -- TODO, nvim-cmp is not eays to config with different languages
   -- https://github.com/neovim/nvim-lspconfig/wiki/Autocompletion
-  -- use 'neovim/nvim-lspconfig' -- Collection of configurations for built-in LSP client
-  -- use 'hrsh7th/nvim-cmp' -- Autocompletion plugin
-  -- use 'hrsh7th/cmp-buffer' -- nvim-cmp source for buffer words
-  -- use 'quangnguyen30192/cmp-nvim-tags' -- tags completion source for nvim-cmp
-  -- use 'hrsh7th/cmp-nvim-lsp' -- LSP source for nvim-cmp
-  -- use 'saadparwaiz1/cmp_luasnip' -- Snippets source for nvim-cmp
-  -- use 'L3MON4D3/LuaSnip' -- Snippets plugin
+  use 'neovim/nvim-lspconfig' -- Collection of configurations for built-in LSP client
+  use 'hrsh7th/nvim-cmp' -- Autocompletion plugin
+  use 'hrsh7th/cmp-nvim-lsp' -- LSP source for nvim-cmp
+  use 'hrsh7th/cmp-path' -- nvim-cmp source for path
+  use 'hrsh7th/cmp-buffer' -- nvim-cmp source for buffer words
+  use 'quangnguyen30192/cmp-nvim-tags' -- tags completion source for nvim-cmp
+  use 'L3MON4D3/LuaSnip' -- Snippets plugin
+  use 'saadparwaiz1/cmp_luasnip' -- Snippets source for nvim-cmp
 
 end)
 
@@ -106,6 +106,13 @@ vim.opt.shiftwidth = 2
 vim.opt.termguicolors = true
 vim.cmd([[colorscheme onedark]])
 
+-- popup menu
+vim.opt.completeopt = {'menu', 'menuone', 'noselect'}
+
+-- listchars
+vim.opt.listchars = {eol = '↵'}
+vim.opt.list = true
+
 -- ========key mapping========
 -- ref: https://github.com/nanotee/nvim-lua-guide#defining-mappings
 
@@ -122,17 +129,32 @@ local wk = require("which-key")
 wk.register({
   -- file
   ["<leader>t"] = { name = "Telescope" },
-  ["<leader>tf"] = { ":Telescope find_files previewer=false<CR>", "Find File" },
-  ["<leader>tb"] = { ":Telescope buffers<CR>", "Find Buffer" },
+  ["<leader>tf"] = { "<cmd>Telescope find_files previewer=false<CR>", "Find File" },
+  ["<leader>tb"] = { "<cmd>Telescope buffers<CR>", "Find Buffer" },
+  ["<leader>td"] = { "<cmd>Telescope lsp_definitions<CR>", "Goto Definitions" },
+  ["<leader>tr"] = { "<cmd>Telescope lsp_references<CR>", "Goto References" },
+  ["<leader>ti"] = { "<cmd>Telescope lsp_implementations<CR>", "Goto Implementations" },
+  ["<leader>tt"] = { "<cmd>Telescope lsp_type_definitions<CR>", "Goto Type Definitions" },
+  -- diagnostics
+  ["<leader>d"] = { name = "Diagnostics" },
+  -- ["<leader>de"] = { "<cmd>lua vim.diagnostic.enable()<CR>", "Enable Diagnostic" },
+  ["<leader>de"] = { "<cmd>LspStart<CR>", "Enable Diagnostic" },
+  -- ["<leader>dd"] = { "<cmd>lua vim.diagnostic.disable()<CR>", "Disable Diagnostic" },
+  ["<leader>dd"] = { "<cmd>LspStop<CR>", "Disable Diagnostic" },
+  ["<leader>dn"] = { "<cmd>lua vim.diagnostic.goto_next()<CR>", "Next Diagnostic" },
+  ["<leader>dp"] = { "<cmd>lua vim.diagnostic.goto_prev()<CR>", "Prev Diagnostic" },
+  ["<leader>dl"] = { "<cmd>Telescope diagnostics<CR>", "List Diagnostic" },
   -- buffer
   ["<leader>b"] = { name = "buffer" },
-  ["<leader>bn"] = { ":bn<CR>", "Next Buffer" },
-  ["<leader>bp"] = { ":bp<CR>", "Previous Buffer" },
-  ["<leader>bd"] = { ":bd<CR>", "Delete Buffer" },
+  ["<leader>bn"] = { "<cmd>bn<CR>", "Next Buffer" },
+  ["<leader>bp"] = { "<cmd>bp<CR>", "Previous Buffer" },
+  ["<leader>bh"] = { "<cmd>lua vim.lsp.buf.hover()<CR>", "Display Info" },
+  ["<leader>brn"] = { "<cmd>lua vim.lsp.buf.rename()<CR>", "Rename" },
+  ["<leader>bc"] = { "<cmd>lua vim.lsp.buf.code_action()<CR>", "Code Action" },
   -- git
   ["<leader>g"] = { name = "git signs" },
-  ["<leader>gt"] = { ":Gitsigns toggle_signs<CR>", "Toggle Signs" },
-  ["<leader>gn"] = { ":Gitsigns next_hunk<CR>", "Next Hunk" },
+  ["<leader>gt"] = { "<cmd>Gitsigns toggle_signs<CR>", "Toggle Signs" },
+  ["<leader>gn"] = { "<cmd>Gitsigns next_hunk<CR>", "Next Hunk" },
   ["<leader>gp"] = { ":Gitsigns prev_hunk<CR>", "Prev Hunk" },
   ["<leader>gd"] = { ":Gitsigns preview_hunk<CR>", "Hunk Diff" },
 })
@@ -182,27 +204,55 @@ require'nvim-treesitter.configs'.setup {
 -- ========LSP and Autocompletion Support========
 -- ==============================================
 
--- -- Add additional capabilities supported by nvim-cmp
--- local capabilities = vim.lsp.protocol.make_client_capabilities()
--- capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
---
--- local lspconfig = require('lspconfig')
---
--- -- Enable some language servers with the additional completion capabilities offered by nvim-cmp
--- -- local servers = { 'clangd', 'rust_analyzer', 'pyright', 'tsserver' }
--- local servers = { 'clangd' }
--- for _, lsp in ipairs(servers) do
---   lspconfig[lsp].setup {
---     -- on_attach = my_custom_on_attach,
---     capabilities = capabilities,
---   }
--- end
---
--- -- luasnip setup
--- local luasnip = require 'luasnip'
---
--- -- nvim-cmp setup
--- local cmp = require 'cmp'
+-- Add additional capabilities supported by nvim-cmp
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
+
+local lspconfig = require('lspconfig')
+
+-- Enable some language servers with the additional completion capabilities offered by nvim-cmp
+-- local servers = { 'clangd', 'rust_analyzer', 'pyright', 'tsserver' }
+local servers = { 'clangd' }
+for _, lsp in ipairs(servers) do
+  lspconfig[lsp].setup {
+    -- on_attach = my_custom_on_attach,
+    capabilities = capabilities,
+  }
+end
+
+-- luasnip setup
+local luasnip = require 'luasnip'
+
+-- nvim-cmp setup
+local cmp = require'cmp'
+cmp.setup({
+  snippet = {
+    -- REQUIRED - you must specify a snippet engine
+    expand = function(args)
+      require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+    end,
+  },
+  mapping = {
+    ['<C-b>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
+    ['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
+    -- ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
+    -- ['<C-y>'] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
+    ['<C-e>'] = cmp.mapping({
+      i = cmp.mapping.abort(),
+      c = cmp.mapping.close(),
+    }),
+    ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+  },
+  sources = cmp.config.sources({
+    { name = 'nvim_lsp' },
+    { name = 'luasnip' }, -- For luasnip users.
+    { name = 'buffer' },
+    { name = 'path' },
+  })
+})
+
+-- with tab
+-- local cmp = require'cmp'
 -- cmp.setup {
 --   snippet = {
 --     expand = function(args)
