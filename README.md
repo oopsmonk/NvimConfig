8564d75f19eb64f5a0f6c294ceed795d340eaf81
oopsmonk <oopsmonk@gmail.com>
2022-06-24 14:56:06 +0800

    removed nerd font

--- README.md
+++ README.md
@@ -1,27 +1,33 @@
-# NVIM Config in Lua
+# NVIM Config in Lua language
+
+Clone the project
+
+```bash
+git clone https://github.com/oopsmonk/NvimConfig.git
+```
 
 Replace local nvim config
 
-```
+```bash
 mv ~/.config/nvim ~/.config/nvim-backup
-ln -s nvimconfig ${HOME}/.config/nvim
+ln -s NvimConfig ${HOME}/.config/nvim
 ```
 
 Install requirments
 
-```
+```bash
 ./install-deps.sh
 ```
 
 Install plugins
 
-```
+```bash
 nvim +PackerInstall
 ```
 
 Check plugin
 
-```
+```bash
 nvim +checkhealth
 ```
 
--- install-deps.sh
+++ install-deps.sh
@@ -2,7 +2,7 @@
 
 function install_deps(){
   echo "Installing dependences..."
-  sudo apt update 
+  sudo apt update
   sudo apt install ripgrep fd-find -y
   if [ $? -ne 0 ]
   then
@@ -23,51 +23,5 @@ function install_deps(){
   fi
 }
 
-function install_nerd_font(){
-  # reqiered by kyazdani42/nvim-web-devicons
-  # install Nerd Font from https://github.com/ryanoasis/nerd-fonts
-  declare -a fonts=(
-      BitstreamVeraSansMono
-      Hack
-      Noto
-      Overpass
-      SourceCodePro
-      Ubuntu
-      UbuntuMono
-  )
-
-  # nerd font release version
-  version='2.1.0'
-
-  fonts_dir="${HOME}/.local/share/fonts"
-
-  if [[ ! -d "$fonts_dir" ]]; then
-      mkdir -p "$fonts_dir"
-  fi
-
-  for font in "${fonts[@]}"; do
-      zip_file="${font}.zip"
-      download_url="https://github.com/ryanoasis/nerd-fonts/releases/download/v${version}/${zip_file}"
-      echo "Downloading $download_url"
-      wget "$download_url"
-      if [ $? -ne 0 ]
-      then
-        echo "download ${font} failed!"
-        exit 1
-      fi
-      unzip "$zip_file" -d "$fonts_dir"
-      rm "$zip_file"
-  done
-
-  # remove unused fonts
-  find "$fonts_dir" -name '*Windows Compatible*' -delete
-
-  # update font cache
-  fc-cache -fv
-}
-
 # call install dependences
 install_deps
-# call install nerd font
-# install_nerd_font
-
