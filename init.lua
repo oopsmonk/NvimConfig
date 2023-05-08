9f6101452ded998c698c6373438a3484d583be3b
oopsoonk <462383+oopsmonk@users.noreply.github.com>
2023-05-08 13:45:23 +0800

    Autocomplition update

--- init.lua
+++ init.lua
@@ -207,7 +207,7 @@ vim.opt.cursorline = true
 vim.api.nvim_create_autocmd("BufReadPost", { command = [[if line("'\"") > 1 && line("'\"") <= line("$") | execute "normal! g`\"" | endif]] })
 
  -- popup menu
--- vim.opt.completeopt = {'menu', 'menuone', 'noselect'}
+vim.opt.completeopt = {'menu', 'menuone', 'noselect'}
 
 -- disable providers
 vim.g.loaded_node_provider = 0
@@ -259,6 +259,40 @@ wk.register({
 
 -- ========LSP and Autocomplition config========
 
+-- nvim-cmp setup
+local cmp = require('cmp')
+cmp.setup({
+  snippet = {
+    -- REQUIRED - you must specify a snippet engine
+    expand = function(args)
+    -- vim.fn["vsnip#anonymous"](args.body) -- For 'vsnip` users
+      require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+    end,
+  },
+  window = {
+  -- completion = cmp.config.window.bordered(),
+  -- documentation = cmp.config.window.bordered(),
+  },
+  mapping = cmp.mapping.preset.insert({
+    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<C-e>'] = cmp.mapping.abort(),
+    ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+  }),
+  sources = cmp.config.sources({
+    { name = 'nvim_lsp' },
+    { name = 'luasnip' }, -- For luasnip users.
+  }, {
+    { name = 'buffer' },
+    { name = 'path' },
+    { name = 'tags' },
+  })
+})
+
+-- Add additional capabilities supported by nvim-cmp
+local cmp_capabilities = require('cmp_nvim_lsp').default_capabilities()
+
 -- IMPORTANT: make sure to setup neodev BEFORE lspconfig
 require("neodev").setup({
   -- add any options here, or leave empty to use the default settings
@@ -285,7 +319,10 @@ local home_dir = os.getenv('HOME')
 local lspconfig = require('lspconfig')
 require("mason-lspconfig").setup_handlers({
   function (server_name)
-    require("lspconfig")[server_name].setup{}
+    require("lspconfig")[server_name].setup{
+      -- nvim-cmp
+      capabilities = cmp_capabilities,
+    }
   end,
   -- mason-lspconfig provides a prebuilt zls which might outdated.
   -- override the path to use the local one
@@ -336,37 +373,6 @@ vim.keymap.set({ "i", "s" }, "<c-k>", function()
     ls.jump(-1)
   end
 end, { silent = true })
-
--- nvim-cmp setup
-local cmp = require'cmp'
-cmp.setup({
-  snippet = {
-    -- REQUIRED - you must specify a snippet engine
-    expand = function(args)
-    -- vim.fn["vsnip#anonymous"](args.body) -- For 'vsnip` users
-      require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
-    end,
-  },
-  window = {
-  -- completion = cmp.config.window.bordered(),
-  -- documentation = cmp.config.window.bordered(),
-  },
-  mapping = cmp.mapping.preset.insert({
-    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-    ['<C-f>'] = cmp.mapping.scroll_docs(4),
-    ['<C-Space>'] = cmp.mapping.complete(),
-    ['<C-e>'] = cmp.mapping.abort(),
-    ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-  }),
-  sources = cmp.config.sources({
-    { name = 'nvim_lsp' },
-    { name = 'luasnip' }, -- For luasnip users.
-  }, {
-    { name = 'buffer' },
-    { name = 'path' },
-    { name = 'tags' },
-  })
-})
 
 -- ========plugin config========
 
